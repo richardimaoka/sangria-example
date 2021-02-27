73880b6419a36cc3bfe007b06d6d896331bc3d15
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-02-28 03:07:34 +0900

    query parser

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,78 +1,46 @@
-/*
- * Copyright (C) 2020-2021 Lightbend Inc. <https://www.lightbend.com>
- */
-
 package example
 
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
-import akka.http.scaladsl.Http
-import akka.Done
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.StatusCodes
-// for JSON serialization/deserialization following dependency is required:
-// "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.DefaultJsonProtocol._
+import sangria.ast.Document
+import sangria.parser.QueryParser
+import sangria.renderer.QueryRenderer
 
-import scala.io.StdIn
+import scala.util.Success
+import scala.util.Failure
+object Main {
+  val query =
+    """
+    query FetchLukeAndLeiaAliased(
+          $someVar: Int = 1.23
+          $anotherVar: Int = 123) @include(if: true) {
+      luke: human(id: "1000")@include(if: true){
+        friends(sort: NAME)
+      }
 
-import scala.concurrent.Future
+      leia: human(id: "princess leia") {
+        name
+      }
 
-object SprayJsonExample {
+      ... on User {
+        birth{day}
+      }
 
-  // needed to run the route
-  implicit val system = ActorSystem(Behaviors.empty, "SprayExample")
-  // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
-  implicit val executionContext = system.executionContext
-
-  var orders: List[Item] = Nil
-
-  // domain model
-  final case class Item(name: String, id: Long)
-  final case class Order(items: List[Item])
-
-  // formats for unmarshalling and marshalling
-  implicit val itemFormat = jsonFormat2(Item)
-  implicit val orderFormat = jsonFormat1(Order)
-
-  // (fake) async database query api
-  def fetchItem(itemId: Long): Future[Option[Item]] = Future {
-    orders.find(o => o.id == itemId)
-  }
-  def saveOrder(order: Order): Future[Done] = {
-    orders = order match {
-      case Order(items) => items ::: orders
-      case _            => orders
+      ...Foo
     }
-    Future { Done }
-  }
 
+    fragment Foo on User @foo(bar: 1) {
+      baz
+    }
+  """
+
+// Parse GraphQL query
   def main(args: Array[String]): Unit = {
-    val route: Route =
-      concat(
-        get {
-          complete("Hello world")
-        },
-        post {
-          path("create-order") {
-            entity(as[Order]) { order =>
-              val saved: Future[Done] = saveOrder(order)
-              onSuccess(saved) {
-                _ => // we are not interested in the result value `Done` but only in the fact that it was successful
-                  complete("order created")
-              }
-            }
-          }
-        }
-      )
+    QueryParser.parse(query) match {
+      case Success(document) =>
+        // Pretty rendering of the GraphQL query as a `String`
+        println(document.renderPretty)
 
-    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+      case Failure(error) =>
+        println(s"Syntax error: ${error.getMessage}")
+    }
   }
 }
